5a73d5229e5bd545289954bcdc669f61438e3c6a
rahyanakil <rahyanakil89@gmail.com>
2025-05-15 16:38:01 +0600

    6 problem solved and added

--- Assignment1/test.ts
+++ Assignment1/test.ts
@@ -1,12 +1,26 @@
 {
   // test1 start
-  function formatString(input: string, toUpper?: boolean): string {
-    if (toUpper === false) {
-      return input.toLowerCase();
-    }
-    return input.toUpperCase();
+enum Day{
+  Monday,
+  Tuesday,
+  Wednesday,
+  Thursday,
+  Friday,
+  Saturday,
+  Sunday
+}
+function getDayType(day:Day):string{
+  if(day ===Day.Saturday || day ===Day.Sunday){
+    return "Weekend";
   }
-  const showString = formatString("Hello", false);
+  return "Weekday"
+}
+
+
+console.log(getDayType(Day.Monday));   // Output: "Weekday"
+console.log(getDayType(Day.Sunday));   // Output: "Weekend"
+console.log(getDayType(Day.Friday));   // Output: "Weekday"
+console.log(getDayType(Day.Saturday));
 
   //test1 end
 }
